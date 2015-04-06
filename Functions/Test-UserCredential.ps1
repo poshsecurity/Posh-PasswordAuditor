64181b81fb0fffc1ec28f33b3a2a20a7376c2147
Kieran Jacobsen <code@poshsecurity.com>
2015-04-06 12:46:30 +1000

    Back to WMI in Test-UserCredential
    Switched back to using WMI for workgroup detection

--- Functions/Test-UserCredential.ps1
+++ Functions/Test-UserCredential.ps1
@@ -1,5 +1,4 @@
-﻿#requires -Version 3
-Set-StrictMode -Version 2
+﻿Set-StrictMode -Version 2
 
 function Test-UserCredential 
 {
@@ -60,8 +59,9 @@ function Test-UserCredential
         2011-08-21: Andy Arismendi - Created.
         2011-08-22: Andy Arismendi - Add pipelining support for Get-Credential.
         2011-08-22: Andy Arismendi - Add support for NTLM/kerberos switch.
-        2015-03-11: Kieran Jacobsen - Removed WMi call to get domain details.
-        2015-03-21: Kieran Jacobsen - General Reformat and move to best practicess.
+        2015-03-11: Kieran Jacobsen - Removed WMI call to get domain details.
+        2015-03-21: Kieran Jacobsen - General Reformat and move to best practice.
+        2015-04-05: Kieran Jacobsen - Restored WMI call as it is more reliable. Additional validation of parameters.
     #>
 
     [CmdletBinding(DefaultParameterSetName = 'UserPass')]
@@ -99,8 +99,14 @@ function Test-UserCredential
         catch
         { throw 'Failed to load assembly "System.DirectoryServices.AccountManagement". The error was: "{0}".' -f $_ }
 
-        if (($ENV:userdomain -eq $ENV:COMPUTERNAME) -and $Domain) 
+        #TODO: Test if the computer is only part of a workgroup, then we should throw an error if -domain is specified.
+        $Computersystem = Get-WmiObject -Class Win32_Computersystem
+        if (($Computersystem.Workgroup -ne $null) -and $Domain)
         { throw 'This computer is not a member of a domain.' }
+
+        # If -domain isn't specified, then we don't need -UseKerberos
+        if (-not $Domain -and $UseKerberos)
+        { throw 'You can only specify -UserKerberos with -Domain'}
     }
 
     Process
@@ -120,7 +126,7 @@ function Test-UserCredential
             { $PasswordText = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($Password)) }
             
             if ($Domain)
-            { $pc = New-Object -TypeName System.DirectoryServices.AccountManagement.PrincipalContext -ArgumentList ('Domain', $ENV:userdomain) } 
+            { $pc = New-Object -TypeName System.DirectoryServices.AccountManagement.PrincipalContext -ArgumentList ('Domain', $Computersystem.Workgroup) } 
             else 
             { $pc = New-Object -TypeName System.DirectoryServices.AccountManagement.PrincipalContext -ArgumentList ('Machine', $ENV:COMPUTERNAME) }
 
@@ -129,7 +135,8 @@ function Test-UserCredential
             else 
             { return $pc.ValidateCredentials($Username, $PasswordText, [DirectoryServices.AccountManagement.ContextOptions]::Negotiate) }
         }
-        catch {throw 'Failed to test user credentials. The error was: "{0}".' -f $_} finally 
+        catch {throw 'Failed to test user credentials. The error was: "{0}".' -f $_} 
+        finally 
         {
             Remove-Variable -Name Username -ErrorAction SilentlyContinue
             Remove-Variable -Name Password -ErrorAction SilentlyContinue
