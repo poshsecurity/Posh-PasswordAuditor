095f8276e87010c4b391edfacfd098efe437b14a
Kieran Jacobsen <code@poshsecurity.com>
2015-04-06 18:06:32 +1000

    Cleanup using ISESteroids

--- Posh-PasswordAuditor.psm1
+++ Posh-PasswordAuditor.psm1
@@ -2,15 +2,13 @@
 # Export the module members - KUDOS to the chocolatey project for this efficent code
 # 
 
-Import-Module ActiveDirectory
+Import-Module -Name ActiveDirectory
 
 #get the path of where the module is saved (if module is at c:\myscripts\module.psm1, then c:\myscripts\)
 $mypath = (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition)
 
 #find all the ps1 files in the subfolder functions
-Resolve-Path -Path $mypath\functions\*.ps1 | ForEach-Object -Process {
-    . $_.ProviderPath
-}
+Resolve-Path -Path $mypath\functions\*.ps1 | ForEach-Object -Process {. $_.ProviderPath }
 
 #export as module members the functions we specify
 Export-ModuleMember -Function Test-UserCredential, Find-UserPassword
